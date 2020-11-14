90d14ee2709b8f4510723b1aef0ef8415e1e9494
Manuel Semeco <manumeco98@gmail.com>
2020-11-14 11:03:47 -0500

    adding routes and REGISTER USER SERVICE

--- soap/soap_pyco/soap/views.py
+++ soap/soap_pyco/soap/views.py
@@ -1,1 +1,45 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.db import IntegrityError
+from django.db.models import F, Sum
+# from django.core.exceptions import ObjectDoesNotExist
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from spyne import Application, rpc, ServiceBase, \
+Integer, Unicode, String, ComplexModel, DateTime, Array, Fault, Boolean, Float
+from spyne import Iterable
+from spyne.protocol.http import HttpRpc
+from spyne.protocol.soap import Soap11
+from spyne.server.django import DjangoApplication
+from django.views.decorators.csrf import csrf_exempt
+from models import *
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+
+class RegisterUser(ServiceBase):
+    @rpc(Unicode,Unicode,Unicode,Unicode,Unicode,Unicode, _returns= String)
+    def register_user(ctx, name, lastname,telephone,password,id_document,email):
+        try:
+            User.objects.create(telephone= telephone,
+                            lastname=lastname,
+                            email=email,
+                            id_document=id_document,
+                            password=generate_password_hash(password),
+                            name=name
+                            )
+            return "true"
+        except IntegrityError as e:
+            raise Fault(faultcode=str(e[0]), faultstring=str(e[1]))
+
+
+application = Application(
+    [RegisterUser],
+    tns='spyne.examples.epayco',
+    in_protocol=Soap11(validator='lxml'),
+    out_protocol=Soap11()
+)
+# This module resides in a package in your Django
+# project.
+payco_app = csrf_exempt(DjangoApplication(application))
