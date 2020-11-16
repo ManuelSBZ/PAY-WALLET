013c2c509f23d4f47489324cb099fcc8ce9415ff
Manuel Semeco <manumeco98@gmail.com>
2020-11-16 18:13:55 -0500

    adding LOGIN_USER ENPOINT

--- api/project_api/app/api/resources.py
+++ api/project_api/app/api/resources.py
@@ -31,4 +31,44 @@ def register_user():
 
     return jsonify({"message":"user created","created":True}) if result =="true"\
                        else make_response(jsonify({"error": str(result), "created":False}),400)
+                       
+@api.route('/getToken', methods=['GET'])
+def login_user():
+    import datetime
+    auth = request.authorization
+    print(f"{auth.password}")
+    if not auth or not auth.username or not auth.password:
+        return make_response(jsonify(
+                    {"message":"missing password or/and username"}), 
+                    401, 
+                    {'WWW.Authentication': 'Basic realm: "login required"'}
+                    )
+    
+    try:
+        owner_verified= client.service.get_user(str(auth.username),
+                                                str(auth.password))
+    except:
+        return make_response(jsonify({"message":"user not verified"}),
+        500, {'WWW.Authentication': 'Basic realm: "login required"'}
+         )
+        print(owner_verified)
+    if owner_verified != "true":
+        return make_response(jsonify(
+        {"message":"wrong password"}),
+         401, {'WWW.Authentication': 'Basic realm: "login required"'}
+         )
+    try:
+        owner = client.service.data_user(str(auth["username"]))
+    except:
+        return make_response(jsonify(
+        {"message":"data_user service fail"}),
+         500, {'WWW.Authentication': 'Basic realm: "login required"'}
+         )
 
+    token = jwt.encode({'id': owner[1],
+                        'email':owner[5],
+                        'exp' : datetime.datetime.utcnow() + 
+                        datetime.timedelta(minutes=60)}, 
+                        current_app.config['SECRET_KEY'])
+
+    return jsonify({'token' : token.decode('UTF-8')})
